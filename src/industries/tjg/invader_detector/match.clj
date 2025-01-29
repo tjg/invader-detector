934a4a8d6abc326d2fc1fe3977867d5799b9046c
tjg <tjg@pentaside.org>
2025-01-29 11:54:15 +0100

    Matches with unequal images and offset.

--- src/industries/tjg/invader_detector/match.clj
+++ src/industries/tjg/invader_detector/match.clj
@@ -1,6 +1,7 @@
 (ns industries.tjg.invader-detector.match
   (:require
-   [clojure.string :as str]))
+   [clojure.string :as str]
+   [industries.tjg.invader-detector.utils :as utils]))
 
 (defn parse-image [image {:keys [chars-true chars-false] :as _opts}]
   (->> image
@@ -65,5 +66,77 @@
   (similarity-score
    [[0 1 0]]
    [[0 1 0]])  ;; => 1
+
+  )
+
+
+(defn similarity-score-at-offset [a b {:keys [b-offset] :as _opts}]
+  (when (not= [(-> a first count)
+               (-> a count)]
+              [(-> b first count)
+               (-> b count)])
+    (ex-info "Array sizes don't match." {:a a :b b}))
+  (let [{:keys [a-start a-size b-start overlap-size]}
+        (utils/overlapping-bounding-boxes a b b-offset)
+
+        matches-arrays
+        (->> (range 0 (:y overlap-size))
+             (mapv (fn [y]
+                     (let [a-array-at-y (get a (+ y (:y a-start)))
+                           b-array-at-y (get b (+ y (:y b-start)))]
+                       (->> (range 0 (:x overlap-size))
+                            (mapv (fn [x]
+                                    (let [a-x (get a-array-at-y (+ x (:x a-start)))
+                                          b-x (get b-array-at-y (+ x (:x b-start)))]
+                                      (if (= a-x b-x) 1 0)))))))))
+
+        matches (for [x (range 0 (:x overlap-size))
+                      y (range 0 (:y overlap-size))]
+                  (= (get-in matches-arrays [y x]) 1))
+
+        match-count (->> matches
+                         (filter identity)
+                         count)]
+    {:effective-sizes [(:x overlap-size) (:y overlap-size)]
+     :a-absolute-size (* (:x a-size) (:y a-size))
+     :match-count match-count
+     :matches-arrays matches-arrays}))
+
+^:rct/test
+(comment
+  (def ^:private test-a
+    [[0 0 0]
+     [0 1 1]
+     [0 1 1]])
+
+  (def ^:private test-b
+    [[1 1 0 0]
+     [1 1 0 0]
+     [0 0 0 0]
+     [0 0 0 0]])
+
+  (similarity-score-at-offset test-a test-b
+                              {:b-offset [0 0]})
+  ;; => {:effective-sizes [3 3]
+  ;;     :a-absolute-size 9
+  ;;     :match-count 3
+  ;;     :matches-arrays [[0 0 1]
+  ;;                      [0 1 0]
+  ;;                      [1 0 0]]}
+
+  (similarity-score-at-offset test-a test-b
+                              {:b-offset [-1 -1]})
+  ;; => {:effective-sizes [2 2]
+  ;;     :a-absolute-size 9
+  ;;     :match-count 4
+  ;;     :matches-arrays [[1 1]
+  ;;                      [1 1]]}
+
+  (similarity-score-at-offset test-a test-b
+                              {:b-offset [-4 -4]})
+  ;; => {:effective-sizes [-1 -1]
+  ;;     :a-absolute-size 9
+  ;;     :match-count 0
+  ;;     :matches-arrays []}
 
   )
---
+++ src/industries/tjg/invader_detector/utils.clj
@@ -0,0 +1,38 @@
+(ns industries.tjg.invader-detector.utils)
+
+(defn overlapping-bounding-boxes [a b [x y :as _b-offset]]
+  (let [[b-offset-x b-offset-y] [x y]
+
+        ;; Actually measured from the datastructures, not calculated.
+        [b-size-x b-size-y][(-> b first count)
+                            (-> b count)]
+        [a-size-x a-size-y] [(-> a first count)
+                             (-> a count)]
+
+        ;; Zero; or if the offset's negative, shift.
+        [a-start-x a-start-y] [(max 0 (- b-offset-x))
+                               (max 0 (- b-offset-y))]
+        ;; Size; or distance from b's start to its end.
+        [a-end-x a-end-y] [(min a-size-x
+                                (- b-size-x b-offset-x))
+                           (min a-size-y
+                                (- b-size-y b-offset-y))]
+
+        ;; We've calculated a's bounds, using a's & b's dimensions. So
+        ;; use it to calculate the effective size of both a & b.
+        [effective-size-x effective-size-y] [(- a-end-x a-start-x)
+                                             (- a-end-y a-start-y)]
+
+        ;; b's offset, or 0 if it's negative.
+        [b-start-x b-start-y] [(max 0 b-offset-x)
+                               (max 0 b-offset-y)]
+
+        ;; b's start + the effective size of a (and b).
+        [b-end-x b-end-y] [(+ b-start-x effective-size-x)
+                           (+ b-start-y effective-size-y)]
+        a-size-total (* a-size-x a-size-y)]
+    {:a-start {:x a-start-x :y a-start-y}
+     :b-start {:x b-start-x :y b-start-y}
+     :a-size {:x a-size-x :y a-size-y}
+     :b-size {:x b-size-x :y b-size-y}
+     :overlap-size {:x effective-size-x :y effective-size-y}}))
