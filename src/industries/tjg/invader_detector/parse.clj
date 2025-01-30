ba0114814edf4da76c83b992410eab42ef566856
tjg <tjg@pentaside.org>
2025-01-30 13:42:36 +0100

    Input validation & parser permissiveness.

--- src/industries/tjg/invader_detector/parse.clj
+++ src/industries/tjg/invader_detector/parse.clj
@@ -2,45 +2,86 @@
   (:require
    [clojure.string :as str]))
 
+(defn- pad-vectors [pad-val vecs]
+  (let [max-size (->> vecs
+                     (map count)
+                     (reduce max 0))]
+    (->> vecs
+         (mapv (fn [v]
+                 (if (= max-size (count v))
+                   v
+                   (let [padded-vector (concat v (repeat pad-val))]
+                     (->> padded-vector
+                          (take max-size)
+                          vec))))))))
+
+^:rct/test
+(comment
+  (pad-vectors 0 [[1] [1 1] [1]])
+  ;; => [[1 0] [1 1] [1 0]]
+  )
+
+
+(defn- parse-char [ch chars-true chars-false line-idx char-idx]
+  (cond (chars-true ch) 1
+        (chars-false ch) 0
+        :else (throw (ex-info
+                      "Unknown character"
+                      {:character ch
+                       :line-idx line-idx
+                       :char-idx char-idx}))))
+
 (defn parse-radar-sample
   "Parse unicode radar data into a 2D vector denoting true/false pixels.
 
   Each line of radar data is separated by newlines.
 
-  `chars-true` & `chars-false` contain characters denoting true or
-  false pixels. An exception is thrown if there's an unknown
-  character."
-  [s {:keys [chars-true chars-false] :as _opts}]
-  (->> s
-       str/split-lines
-       (map-indexed (fn [line-idx line]
-                      (->> line
-                           (map-indexed (fn [char-idx c]
-                                          (cond ((set chars-true) c)
-                                                1
-                                                ((set chars-false) c)
-                                                0
-                                                :else
-                                                (throw (ex-info
-                                                        "Unknown character"
-                                                        {:character c
-                                                         :line-idx line-idx
-                                                         :char-idx char-idx})))))
-                           vec)))
-       vec))
+  `opts` can contain the following keys:
+  - `:chars-true` & `:chars-false`: contains characters denoting true or
+     false pixels, respectively.
+  - `:pad-lines?`: if true, pad lines with false pixels so they're all
+     equal lengths.
+
+  An exception is thrown if `s` contains an unknown character."
+  [s {:keys [chars-true chars-false pad-lines?]
+      :or {chars-true [\o \O] chars-false [\-]}
+      :as _opts}]
+  (let [chars-true (set chars-true)
+        chars-false (set chars-false)
+        possibly-pad-vectors (fn [vs]
+                               (if pad-lines?
+                                 (pad-vectors 0 vs)
+                                 vs))]
+    (->> s
+         str/split-lines
+         (map-indexed
+          (fn [line-idx line]
+            (->> line
+                 (map-indexed (fn [char-idx ch]
+                                (parse-char ch chars-true chars-false
+                                            line-idx char-idx)))
+                 vec)))
+         possibly-pad-vectors
+         vec)))
 
 (defn parse-radar-sample-from-file
   "Parse unicode radar data into a 2D vector denoting true/false pixels.
 
   Each line of radar data is separated by newlines.
 
-  `chars-true` & `chars-false` contain characters denoting true or
-  false pixels. An exception is thrown if there's an unknown
-  character."
-  [file]
-  (-> file
-      slurp
-      (parse-radar-sample {:chars-true [\o \O] :chars-false [\-]})))
+  `opts` can contain the following keys:
+  - `:chars-true` & `:chars-false`: contains characters denoting true or
+     false pixels, respectively.
+  - `:pad-lines?`: if true, pad lines with false pixels so they're all
+     equal lengths.
+
+  An exception is thrown if the file contains an unknown character."
+  ([file]
+   (parse-radar-sample-from-file file {}))
+  ([file opts]
+   (-> file
+       slurp
+       (parse-radar-sample opts))))
 
 ^:rct/test
 (comment
---
+++ src/industries/tjg/invader_detector/validate.clj
@@ -0,0 +1,49 @@
+(ns industries.tjg.invader-detector.validate
+  (:require
+   [clojure.pprint :as pprint]
+   [clojure.set :as set]
+   [clojure.string :as str]
+   [malli.core :as m]
+   [malli.error :as me]))
+
+(defn- uniform-line-length? [s]
+  (let [line-lengths (->> s str/split-lines (map count) set)]
+    (= 1 (count line-lengths))))
+
+(defn- invalid-chars [s ok-chars]
+  (set/difference (set s) (set ok-chars)))
+
+(defn make-radar-sample-schema
+  "Malli schema for radar samples."
+  [{:keys [ok-chars pad-lines?]}]
+  (let [ok-chars (set/union (set ok-chars) #{\newline \return})]
+    [:and
+     string?
+     [:fn {:error/message "all lines should have the same length"}
+      (fn [s] (or pad-lines?
+                  (uniform-line-length? s)))]
+     [:fn
+      {:error/fn
+       {:en (fn [{:keys [value]} _]
+              (let [bad-chars (invalid-chars value ok-chars)]
+                (pprint/cl-format
+                 nil "contains invalid chars: ~{'~a'~^, ~}" bad-chars)))}}
+      #(empty? (invalid-chars % ok-chars))]]))
+
+
+^:rct/test
+(comment
+  (me/humanize (m/explain (make-radar-sample-schema {:ok-chars #{\. \newline \return}
+                                             :pad-lines? true})
+                          "..\n'?"))
+  ;; => ["contains invalid chars: ''', '?'"]
+
+  (me/humanize (m/explain (make-radar-sample-schema {:ok-chars #{\. \newline \return}})
+                          "..\n."))
+  ;; => ["all lines should have the same length"]
+
+  (m/validate (make-radar-sample-schema {:ok-chars #{\. \newline \return}})
+              "..\n..")
+  ;; => true
+
+  )
