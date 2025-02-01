6cf7ee19374c3c29158522d23186691a73e841cc
tjg <tjg@pentaside.org>
2025-02-01 18:30:45 +0100

    Refactor: 1-arity fn options.

--- src/industries/tjg/invader_detector/run.clj
+++ src/industries/tjg/invader_detector/run.clj
@@ -10,71 +10,98 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Utils
 
-(defn- get-matches [invaders radar
-                    {:keys [score-threshold max-results]}]
-  (cond->> invaders
-    true        (mapcat
-                 (fn [{:keys [invader-id pixel-matrix]}]
-                   (->> (match/matches pixel-matrix radar)
-                        (map #(assoc % :invader-id invader-id)))))
-    true        (sort-by :score)
-    max-results (take-last max-results)
-    true        (filter (fn [{:keys [score]}]
-                          (>= score
-                              (/ score-threshold 100))))))
+(defn- get-matches
+  ([{:keys [invaders radar] :as opts}]
+   (get-matches invaders radar  opts))
+  ([invaders radar {:keys [score-threshold max-results]}]
+   (cond->> invaders
+     true        (mapcat
+                  (fn [{:keys [invader-id pixel-matrix]}]
+                    (->> (match/matches pixel-matrix radar)
+                         (map #(assoc % :invader-id invader-id)))))
+     true        (sort-by :score)
+     max-results (take-last max-results)
+     true        (filter (fn [{:keys [score]}]
+                           (>= score
+                               (/ score-threshold 100)))))))
 
-(defn- draw-ascii! [radar matches {:keys [output-ascii
-                                          output-on-char
-                                          output-off-char
-                                          output-opaque-fill]}]
-  (let [draw-opts (merge {:label-offset {:x 1, :y 0}}
-                         (when output-on-char
-                           {:char-true output-on-char})
-                         (when output-off-char
-                           {:char-false output-off-char})
-                         (when output-opaque-fill
-                           {:transparent-fill? false}))]
-    (-> (emit/draw-pixel-matrix radar draw-opts)
-        (emit/draw-scoreboxes matches draw-opts)
-        (emit/save-to-file! output-ascii {}))))
+(defn- draw-ascii!
+  ([{:keys [matches] :as opts}]
+   (draw-ascii! matches  opts))
+  ([radar matches {:keys [output-ascii
+                          output-on-char
+                          output-off-char
+                          output-opaque-fill]}]
+   (let [draw-opts (merge {:label-offset {:x 1, :y 0}}
+                          (when output-on-char
+                            {:char-true output-on-char})
+                          (when output-off-char
+                            {:char-false output-off-char})
+                          (when output-opaque-fill
+                            {:transparent-fill? false}))]
+     (-> (emit/draw-pixel-matrix radar draw-opts)
+         (emit/draw-scoreboxes matches draw-opts)
+         (emit/save-to-file! output-ascii {})))))
 
-(defn- draw-images! [invaders radar matches {:keys [output-images invader-colors]}]
-  (let [invader-color-table (->> [(->> invaders
-                                       (map :invader-id))
-                                  (cycle invader-colors)]
-                                 (apply map vector)
-                                 (into {}))
-        colored-matches (->> matches
-                             (map (fn [{:keys [invader-id] :as match}]
-                                    (assoc match
-                                           :color (invader-color-table invader-id)))))
-        img (-> (image/draw-pixel-matrix radar {})
-                (image/draw-scoreboxes colored-matches {}))]
-    (doseq [filename output-images]
-      (let [image-format (-> filename
-                             (str/split #"\.")
-                             last)]
-        (image/save-to-file! img filename
-                             {:image-format image-format})))))
+(defn- draw-images!
+  ([{:keys [invaders radar matches] :as opts}]
+   (draw-images! invaders radar matches  opts))
+  ([invaders radar matches {:keys [output-images invader-colors]}]
+   (let [invader-color-table (->> [(->> invaders
+                                        (map :invader-id))
+                                   (cycle invader-colors)]
+                                  (apply map vector)
+                                  (into {}))
+         colored-matches (->> matches
+                              (map (fn [{:keys [invader-id] :as match}]
+                                     (assoc match
+                                            :color (invader-color-table invader-id)))))
+         img (-> (image/draw-pixel-matrix radar {})
+                 (image/draw-scoreboxes colored-matches {}))]
+     (doseq [filename output-images]
+       (let [image-format (-> filename
+                              (str/split #"\.")
+                              last)]
+         (image/save-to-file! img filename
+                              {:image-format image-format}))))))
 
-(defn- save-matches-to-file! [matches {:keys [save-matches]}]
-  (let [edn (->> matches
-                 (map #(select-keys % [:invader-id :bbox :score]))
-                 utils/format-edn)]
-    (spit save-matches edn)))
+(defn- save-matches-to-file!
+  ([{:keys [matches] :as opts}]
+   (save-matches-to-file! matches opts))
+  ([matches {:keys [save-matches]}]
+   (let [edn (->> matches
+                  (map #(select-keys % [:invader-id :bbox :score]))
+                  utils/format-edn)]
+     (spit save-matches edn))))
 
-(defn- parse-radar-sample [file {:keys [input-lenient-parsing]}]
-  (parse/parse-radar-sample-from-file
-   file
-   (if input-lenient-parsing
-     {:pad-lines? true, :chars-false-by-default true}
-     {})))
+(defn- parse-radar-sample
+  ([{:keys [radar-sample-file] :as opts}]
+   (parse-radar-sample radar-sample-file opts))
+  ([file {:keys [input-lenient-parsing]}]
+   (parse/parse-radar-sample-from-file
+    file
+    (if input-lenient-parsing
+      {:pad-lines? true, :chars-false-by-default true}
+      {}))))
 
-(defn- print-matches! [matches _opts]
-  (->> matches
-       (map #(select-keys % [:invader-id :bbox :score]))
-       utils/format-edn
-       print))
+(defn- parse-invaders
+  ([{:keys [invader-files] :as opts}]
+   (parse-invaders invader-files opts))
+  ([invader-files opts]
+   (->> invader-files
+        (map #(parse-radar-sample % opts))
+        (map-indexed (fn [id invader]
+                       {:invader-id id
+                        :pixel-matrix invader})))))
+
+(defn- print-matches!
+  ([{:keys [matches] :as opts}]
+   (print-matches! matches opts))
+  ([matches _opts]
+   (->> matches
+        (map #(select-keys % [:invader-id :bbox :score]))
+        utils/format-edn
+        print)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Public API
@@ -95,11 +122,7 @@
     :as opts}]
 
   (let [ ;; Parse input files.
-        invaders (->> invader-files
-                      (map #(parse-radar-sample % opts))
-                      (map-indexed (fn [id invader]
-                                     {:invader-id id
-                                      :pixel-matrix invader})))
+        invaders (parse-invaders invader-files opts)
         radar (parse-radar-sample radar-sample-file opts)
 
         ;; Matches.
